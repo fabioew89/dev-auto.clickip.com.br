cf212b6d450deabb376ee9a79470d397562838f9
Fabio Ewerton <fabioew89@gmail.com>
2024-10-03 23:46:30 -0400

    modified:   app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index/')
 def principal(debug=True):
-    return "<h1>Hello Wolrd!</h1>"
+    fruta1 = 'uva'
+    fruta2 = 'maca'
+    fruta3 = 'laranja'
+    fruta4 = 'morango'
+    return render_template('index.html', 
+                           fruta1=fruta1,
+                           fruta2=fruta2,
+                           fruta3=fruta3,
+                           fruta4=fruta4)
+
+@app.route('/sobre/')
+def sobre():
+    return render_template('sobre.html')
+
+# http://localhost:5000
