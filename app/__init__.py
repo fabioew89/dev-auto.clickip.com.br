bd283510a1cec016e695617a616c7b2bafcb603f
Fábio Ewerton <fabioew89@gmail.com>
2024-10-11 14:01:54 -0400

    modified:   app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from livereload import Server
 
 app = Flask(__name__)
 
 from app.controllers import routes
+
+server = Server(app.wsgi_app)
+server.watch('app/templates/*.*')
+server.watch('app/static/*.*')
+server.serve(port=5000)
