14b9e80185d543930807f2d11f60eef9b1d5f0de
Fábio Ewerton <fabioew89@gmail.com>
2024-10-11 09:02:36 -0400

    modified:   app/controllers/netmiko-handle.py

--- app/controllers/netmiko-handle.py
+++ app/controllers/netmiko-handle.py
@@ -2,14 +2,17 @@ from netmiko import ConnectHandler
 from getpass import getpass
 
 vroute = {
-    'device_type':'juniper',
-    'password':getpass(f'senha: '),
+    'device_type':'juniper',''
+    'host':input(f'host: '),
+    'username': input(f'username: '),
+    'password': getpass(f'senha: '),
     # 'port':'22',
     # 'secret':''
 }
 
 ssh = ConnectHandler(**vroute)
 
+print()
 print(ssh.find_prompt())
 print(ssh.send_command('show interfaces terse lo0 | match lo'))
 ssh.disconnect()
