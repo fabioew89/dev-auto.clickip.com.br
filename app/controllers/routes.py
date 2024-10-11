0be1e86fd8c4e35a07d7a357dd9a3c3c5212979c
Fábio Ewerton <fabioew89@gmail.com>
2024-10-11 16:40:53 -0400

    modified:   app/controllers/routes.py

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -2,11 +2,9 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index/')
 def index():
     return render_template('index.html')
 
-@app.route('/sobre/')
-@app.route('/about/')
+@app.route('/about')
 def about():
-    return render_template('sobre.html')
+    return render_template('about.html')
