84a4ed2c7b9f9a451ac74d6579c553c9945721db
Fábio Ewerton <fabioew89@gmail.com>
2024-10-14 16:07:57 -0400

    modified:   app/controller/routes.py

--- app/controller/routes.py
+++ app/controller/routes.py
@@ -1,18 +1,21 @@
 from flask import render_template, request
+from app.controller.netmiko import sh_int_terse
 from app import app
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        host = request.form['host']
-        username = request.form['username']
-        password = request.form['password']
-        result = sh_int_terse('host','username','password')
+        host = request.form.get('host')
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-        return render_template('index.html', result=result)
+        output = sh_int_terse(host, username, password)
 
+        return render_template('index.html', output=output)
+    
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
