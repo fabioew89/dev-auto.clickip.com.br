eb82015780d651388b46a50810caed8ffdbf1549
Fábio Ewerton <fabioew89@gmail.com>
2024-10-16 17:06:46 -0400

    modified:   app/controller/routes.py

--- app/controller/routes.py
+++ app/controller/routes.py
@@ -1,22 +1,31 @@
 from flask import Flask, render_template, request
-from app.controller.netmiko import sh_int_terse
+from app.controller import netmiko
 from app import app
 
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         host = request.form.get('host')
         username = request.form.get('username')
         password = request.form.get('password')
-
-        output = sh_int_terse(host, username, password)
-
+        output = netmiko.sh_int_terse(host, username, password)
         return render_template('index.html', output=output)
-    
     return render_template('index.html')
 
+@app.route('/unit', methods=['GET', 'POST'])
+def sh_config_int_unit():
+    if request.method == 'POST':
+        host = request.form.get('fhost')
+        username = request.form.get('fusername')
+        password = request.form.get('fpassword')
+        unit = request.form.get('funit')
+
+        output = netmiko.sh_config_int_unit(host, username, password, unit)
+        return render_template('sh_config_int_unit.html', output=output)
+    return render_template('sh_config_int_unit.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return 'about'
