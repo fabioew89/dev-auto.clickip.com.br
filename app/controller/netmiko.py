6a37d5ddb947a62aa25ee09e6fbecadf785e26ea
Fábio Ewerton <fabioew89@gmail.com>
2024-10-16 17:05:47 -0400

    modified:   app/controller/netmiko.py

--- app/controller/netmiko.py
+++ app/controller/netmiko.py
@@ -15,3 +15,19 @@ def sh_int_terse(host, username, password):
     ssh.disconnect()
 
     return output
+
+def sh_config_int_unit(host, username, password, unit):
+    vroute = {
+        'device_type':'juniper',''
+        'host': host,
+        'username': username,
+        'password': password,
+    }
+
+    ssh = ConnectHandler(**vroute)
+
+    # hostname = ssh.find_prompt()
+    output = ssh.send_command(f'show configuration interfaces ae0 unit { unit }')
+    ssh.disconnect()
+
+    return output
