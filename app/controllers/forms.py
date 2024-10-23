3d06f4ed1308ba1bf34666d5eccd19b8eb984f1f
Fabio Ewerton <fabioew89@gmail.com>
2024-10-22 23:57:09 -0400

    modified:   app/controllers/forms.py

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
 
-class CadastroForm(FlaskForm):
-    username = StringField(label='username')
-    email = EmailField(label='email')
-    password = PasswordField(label='password')
-    password_confirm = PasswordField(label='confir password')
-    submit = SubmitField(label='Enviar')
+class Form_Cad_User(FlaskForm):
+    username = StringField(name='username', id='username', validators=[DataRequired(), Length(min=4, max=32)])
+    email = EmailField(name='email', id='email', validators=[DataRequired(), Length(min=4, max=32)])
+    password = PasswordField(name='password', id='password', validators=[DataRequired(), Length(min=6)])
+    password_confirm = PasswordField(name='password_confirm', id='password_confirm', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField(validators=[DataRequired()])
