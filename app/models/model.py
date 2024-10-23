b5b9f07cdaa18d08af580618a54bc0a0e53dbc19
Fábio Ewerton <fabioew89@gmail.com>
2024-10-23 17:56:32 -0400

    modified:   app/models/model.py

--- app/models/model.py
+++ app/models/model.py
@@ -2,7 +2,12 @@ from app import db
 from sqlalchemy.orm import Mapped, mapped_column
 
 class Tab_User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, unique=True, nullable=False)
-    username: Mapped[str] = mapped_column(unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    username: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    # password: Mapped[str] = mapped_column(nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
