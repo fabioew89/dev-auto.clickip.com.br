77d6dd5dd19169bbf9a5c6fba59f1206c5309b7e
Fabio Ewerton <fabioew89@gmail.com>
2024-10-22 22:18:24 -0400

    modified:   app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from livereload import Server
 
 app = Flask(__name__)
 
@@ -10,13 +9,14 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 
-app.config['SECRET_KEY'] = 'f6b42562bc1f3ee92dbad7c9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['SECRET_KEY'] = 'f6b42562bc1f3ee92dbad7c9'
 db.init_app(app)
 
 from app.controllers import routes
 
 # # Only for frontend!
+# from livereload import Server
 # server = Server(app.wsgi_app)
 # server.watch('app/templates/*.*')
 # server.watch('app/static/**/*.*')
