52aed9a38bea809db553ff8c1d90668bec713a43
Fábio Ewerton <fabioew89@gmail.com>
2024-10-25 17:22:09 -0400

    modified:   app/controllers/forms.py

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class Form_Cad_User(FlaskForm):
-    username = StringField(validators=[DataRequired(), Length(min=4, max=32)])
-    email = EmailField(validators=[DataRequired(), Length(min=4, max=32)])
-    submit = SubmitField('Commit')
+    username = StringField(validators=[DataRequired(), Length(min=3, max=20)])
+    email = EmailField(validators=[DataRequired(), Email(),Length(min=15, max=35)])
+    submit = SubmitField('Salvar') # dont count like column
 
 class Form_Cad_Future(FlaskForm):
     username = StringField(validators=[DataRequired(), Length(min=4, max=32)])
-    email = EmailField(validators=[DataRequired(), Length(min=4, max=32)])
+    email = EmailField(validators=[DataRequired(), Email(), Length(min=4, max=32)])
     password = PasswordField(validators=[DataRequired(), Length(min=6)])
     password_confirm = PasswordField(validators=[
         DataRequired(), 
