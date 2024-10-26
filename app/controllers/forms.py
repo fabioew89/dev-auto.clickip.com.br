bb39ef5bb98c66503798583018b7bdba37d4d81e
Fabio Ewerton <fabioew89@gmail.com>
2024-10-25 20:30:51 -0400

    modified:   app/controllers/forms.py

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from app.models.model import Tab_User
 
 class Form_Cad_User(FlaskForm):
+    def validate_username(self, check_user):
+        user = Tab_User.query.filter_by(username=check_user.data).first()
+        if user:
+            raise ValidationError('Usuario ja cadastrado!!!')
+
+    def validate_email(self, check_email):
+        email = Tab_User.query.filter_by(email=check_email.data).first()
+        if email:
+            raise ValidationError('Email ja cadastrado!!!')
+              
     username = StringField(validators=[DataRequired(), Length(min=3, max=20)])
     email = EmailField(validators=[DataRequired(), Email(),Length(min=15, max=35)])
     submit = SubmitField('Salvar') # dont count like column
@@ -11,10 +22,7 @@ class Form_Cad_Future(FlaskForm):
     username = StringField(validators=[DataRequired(), Length(min=4, max=32)])
     email = EmailField(validators=[DataRequired(), Email(), Length(min=4, max=32)])
     password = PasswordField(validators=[DataRequired(), Length(min=6)])
-    password_confirm = PasswordField(validators=[
-        DataRequired(), 
-        Length(min=6),
-        EqualTo('password', message='Password must match')
+    password_confirm = PasswordField(validators=[DataRequired(),Length(min=6),EqualTo('password', message='Password must match')
     ])
     submit = SubmitField('Cadastrar')
 
