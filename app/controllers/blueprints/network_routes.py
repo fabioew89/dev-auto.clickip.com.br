50e4c9f1957cd3cad3b93737e0a7ec6fa63de5fa
Fábio Ewerton <fabioew89@gmail.com>
2024-11-18 15:36:12 -0400

    app/controllers/blueprints/network_routes.py

--- app/controllers/blueprints/network_routes.py
+++ app/controllers/blueprints/network_routes.py
@@ -1,31 +1,45 @@
-from app import db
-from app.models.model import Table_Register
+from app import app, db
 from app.controllers.netmiko import netmiko
+from app.models.model import Table_Register, Table_Devices
 from flask import Blueprint, request, render_template
 
 network_bp = Blueprint('network',__name__)
 
-# username = db.session.execute(db.select(Table_Register).filter_by(username=username)).scalar_one_or_none()
+with app.app_context():
+    users = db.session.execute(db.select(Table_Register)).scalars().all()
+    devices = db.session.execute(db.select(Table_Devices)).scalars().all()
 
-# Mostra a configuração de uma interface do Juniper - get_interface_configuration
-@network_bp.route('/get_interface_configuration', methods=['GET','POST'])
-def get_interface_configuration():
-    if request.method == 'POST':
-        hostname = request.form.get('hostname')
-        username = request.form.get('username')
-        password = request.form.get('password')
-        unit = request.form.get('unit')
-        output = netmiko.get_interface_summary(hostname, username, password, unit)
-        return render_template('router/get_interface_configuration.html', output=output)
-    return render_template('router/get_interface_configuration.html')
-
+# get_interface_summary
 @network_bp.route('/get_interface_summary', methods=['GET','POST'])
 def get_interface_summary():
+    
+    output = None
+    
     if request.method == 'POST':
         hostname = request.form.get('hostname')
         username = request.form.get('username')
         password = request.form.get('password')
         
         output = netmiko.get_interface_summary(hostname, username, password)
-        return render_template('router/get_interface_summary.html', output=output)
-    return render_template('router/get_interface_summary.html')
+        
+    return render_template('router/get_interface_summary.html', 
+                               output=output, 
+                               users=users, 
+                               devices=devices)
+
+# get_interface_configuration
+@network_bp.route('/get_interface_configuration', methods=['GET','POST'])
+def get_interface_configuration():
+    
+    output = None
+    
+    if request.method == 'POST':
+        hostname = request.form.get('hostname')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        unit = request.form.get('unit')
+        output = netmiko.get_interface_summary(hostname, username, password, unit)
+    return render_template('router/get_interface_configuration.html', 
+                               output=output, 
+                               users=users, 
+                               devices=devices)
