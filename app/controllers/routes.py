5e840f62314a7122756a71723052570c8bcfa3eb
Fábio Ewerton <fabioew89@gmail.com>
2024-11-21 16:58:56 -0400

    modified:   app/controllers/routes.py

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -18,93 +18,19 @@ def page_home():
 ##### ##### ##### ##### REGISTER ## ##### ##### ##### ##### 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 
-@app.route('/register', methods=['GET', 'POST'])
-def page_register():
-    form = Form_Register()
-    table = db.session.execute(db.select(Table_Register)).scalars().all()
-    
-    if form.validate_on_submit():
-        
-        user = Table_Register(
-            username = form.username.data,
-            password = generate_password_hash(form.password.data, method='pbkdf2:sha256')
-        )
-        db.session.add(user)
-        db.session.commit()
-        flash('Thanks for registering', category='success')
-        return redirect(url_for('page_register'))
-    
-    if form.errors != {}:
-        for err in form.errors.values():
-            flash(f' Erro ao cadastrar usuario {err}', category='danger')
-    
-    return render_template('page_register_user.html', form=form, table=table)
+
+
+
 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 
-@app.route('/<int:id>/edit_user', methods=['GET', 'POST'])
-def page_edit_user(id):
-    user = db.session.execute(db.select(Table_Register).filter_by(id=id)).scalar_one_or_none()
-    form = Form_Register(obj=user)
-    
-    if user is None:
-        flash(f'Usuário com ID {id} não encontrado.', category='danger')
-        return redirect(url_for('page_home'))
-    
-    if form.validate_on_submit():
-        # Adicionando print para verificar o valor
-        print("Username recebido do form:", form.username.data)
-        user.username = form.username.data
-        user.password = form.password.data
-        user.password_confirm = form.password.data
-        db.session.commit()
-        flash('Usuário atualizado com sucesso!', category='success')
-        return redirect(url_for('page_register'))
-    
-    return render_template('page_edit_user.html', user=user, form=form)
 
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
-
-@app.route('/<int:id>/remove_user')
-def remove_user(id):
-    device = db.session.execute(db.select(Table_Register).filter_by(id=id)).scalar_one_or_none()
-    
-    if device:
-        db.session.delete(device)
-        db.session.commit()
-        flash('User excluído com sucesso.', category='success')
-    else:
-        flash('Device não encontrado.', category='danger')
-
-    return redirect(url_for('page_register'))
 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 ##### ##### ##### ##### ## LOGIN ## ##### ##### ##### ##### 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 
-@app.route('/login', methods=['GET', 'POST'])
-def page_login():
-    form = Form_Login()
-    
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        
-        user = db.session.execute(db.select(Table_Register).filter_by(username=username)).scalar_one_or_none()
 
-        if user and check_password_hash(user.password, password):
-            login_user(user)
-            flash(f'Sucesso ao logar {username}', category='success')
-            return redirect(url_for('page_home'))
-        else:
-            flash('Email ou senha incorretos', category='danger')
-    
-    if form.errors:
-        for field_name, err_messages in form.errors.items():
-            for err in err_messages:
-                flash(f'Erro no campo {field_name}: {err}', category='danger')
-
-    return render_template('login.html', form=form)
 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 ##### ##### ##### ### REGISTER DEVICE ### ##### ##### ##### 
@@ -170,43 +96,11 @@ def remove_device(id):
 ##### ##### ##### ##### ## LOGOUT # ##### ##### ##### ##### 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 
-@app.route('/logout')
-def page_logout():
-    logout_user()
-    flash(f'Deslogado!!!', category='info')
-    return redirect(url_for('page_home'))
+
 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 ##### ##### ##### ##### # NETMIKO # ##### ##### ##### ##### 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
-
-@app.route('/unit', methods=['GET', 'POST'])
-def sh_config_int_unit():
-    if request.method == 'POST':
-        host = request.form.get('fhost')
-        username = request.form.get('fusername')
-        password = request.form.get('fpassword')
-        unit = request.form.get('funit')
-        output = netmiko.sh_config_int_unit(host, username, password, unit)
-        return render_template('sh_config_int_unit.html', output=output)
-    return render_template('sh_config_int_unit.html')
-
-# @app.route('/', methods=['GET', 'POST'])
-# def page_home():
-#     if request.method == 'POST':
-#         host = request.form.get('host')
-#         username = request.form.get('username')
-#         password = request.form.get('password')
-#         output = netmiko.sh_int_terse(host, username, password)
-#         return render_template('home.html', output=output)
-#     return render_template('home.html')
-
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
-##### ##### ##### ##### ## HOME ### ##### ##### ##### ##### 
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
-
-# @app.route('/')
-# def page_home():
 
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 ##### ##### ##### ##### ### END ### ##### ##### ##### ##### 
