cec6db010021e8a7f7b549421d0a901290a7e70a
Fábio Ewerton <fabioew89@gmail.com>
2024-11-22 09:14:41 -0400

    fix: ajuste nas url

--- app/controllers/blueprints/user_routes.py
+++ app/controllers/blueprints/user_routes.py
@@ -6,15 +6,14 @@ from werkzeug.security import generate_password_hash
 
 user_bp = Blueprint('user', __name__)
 
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
-
-@user_bp.route('/register', methods=['GET', 'POST'])
+# Página para registro de usuários
+@user_bp.route('/create', methods=['GET', 'POST'])
 def page_register():
     form = Form_Register()
     table = db.session.execute(db.select(Table_Register)).scalars().all()
     
     if form.validate_on_submit():
-        # Criação do usuário com senha hash
+        # Criação de um novo usuário com senha criptografada
         user = Table_Register(
             username=form.username.data,
             password=generate_password_hash(form.password.data, method='pbkdf2:sha256')
@@ -24,18 +23,17 @@ def page_register():
         flash('Usuário cadastrado com sucesso!', category='success')
         return redirect(url_for('user.page_register'))
     
-    # Exibe erros do formulário, caso existam
+    # Exibe mensagens de erro do formulário, se houver
     for errors in form.errors.values():
         for error in errors:
             flash(f'Erro ao cadastrar usuário: {error}', category='danger')
     
     return render_template('user/page_register_user.html', form=form, table=table)
 
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
-
-@user_bp.route('/<int:id>/edit_user', methods=['GET', 'POST'])
+# Página para edição de usuários
+@user_bp.route('/<int:id>/edit', methods=['GET', 'POST'])
 def page_edit_user(id):
-    # Busca o usuário no banco
+    # Busca o usuário no banco de dados
     user = db.session.execute(db.select(Table_Register).filter_by(id=id)).scalar_one_or_none()
     if user is None:
         flash(f'Usuário com ID {id} não encontrado.', category='danger')
@@ -47,7 +45,7 @@ def page_edit_user(id):
     if form.validate_on_submit():
         # Atualização dos dados do usuário
         user.username = form.username.data
-        if form.password.data:  # Apenas atualiza a senha se um valor for fornecido
+        if form.password.data:  # Atualiza a senha apenas se for fornecida
             user.password = generate_password_hash(form.password.data, method='pbkdf2:sha256')
         db.session.commit()
         flash('Usuário atualizado com sucesso!', category='success')
@@ -55,17 +53,17 @@ def page_edit_user(id):
 
     return render_template('user/page_edit_user.html', user=user, form=form)
 
-########## ########## ########## ########## ########## 
-
-@user_bp.route('/<int:id>/remove_user')
+# Página para remoção de usuários
+@user_bp.route('/<int:id>/del')
 def remove_user(id):
-    # Busca o usuário no banco
+    # Busca o usuário no banco de dados
     user = db.session.execute(db.select(Table_Register).filter_by(id=id)).scalar_one_or_none()
     
     if user:
+        # Remove o usuário do banco de dados
         db.session.delete(user)
         db.session.commit()
-        flash('Usuário excluído com sucesso.', category='success')
+        flash('Usuário excluído com sucesso!', category='success')
     else:
         flash('Usuário não encontrado.', category='danger')
     
