1a705ec911400085676a1588990a79b765c48627
Fábio Ewerton <fabioew89@gmail.com>
2024-11-25 12:50:15 -0400

    fix: formatting according to flake8

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -2,8 +2,9 @@ from app import db
 from flask_wtf import FlaskForm
 from app.models.model import Table_Register
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, \
-    ValidationError, IPAddress
+from wtforms.validators import (
+    DataRequired, Length, EqualTo, ValidationError, IPAddress
+)
 
 
 class Form_Register(FlaskForm):
@@ -11,32 +12,58 @@ class Form_Register(FlaskForm):
         username = db.session.execute(
             db.select(Table_Register).filter_by(username=field.data)
         ).scalar_one_or_none()
-
         if username:
             raise ValidationError('Usuário já cadastrado')
 
-    username = StringField('Username', validators=[
-        DataRequired(),
-        Length(min=3, max=25)
-        ])
-    password = PasswordField('Password', validators=[
-        DataRequired(),
-        Length(min=6)
-        ])
-    password_confirm = PasswordField('Confirm Password', validators=[
-        DataRequired(),
-        EqualTo('password', message='As senhas devem coincidir')
-        ])
+    username = StringField(
+        'Username',
+        validators=[
+            DataRequired(),
+            Length(min=3, max=25),
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=6),
+        ]
+    )
+    password_confirm = PasswordField(
+        'Confirm Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='As senhas devem coincidir'),
+        ]
+    )
     submit = SubmitField('Cadastrar')
 
 
 class Form_Login(FlaskForm):
-    username = StringField(validators=[DataRequired()])
-    password = PasswordField(validators=[DataRequired()])
+    username = StringField(
+        validators=[
+            DataRequired(),
+        ]
+    )
+    password = PasswordField(
+        validators=[
+            DataRequired(),
+        ]
+    )
     submit = SubmitField('Entrar')
 
 
 class Form_Devices(FlaskForm):
-    hostname = StringField(validators=[DataRequired(), Length(min=3, max=10)])
-    ip_address = StringField(validators=[DataRequired(), IPAddress()])
+    hostname = StringField(
+        validators=[
+            DataRequired(),
+            Length(min=3, max=10),
+        ]
+    )
+    ip_address = StringField(
+        validators=[
+            DataRequired(),
+            IPAddress(),
+        ]
+    )
     submit = SubmitField('Cadastrar')
