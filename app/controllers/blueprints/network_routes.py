ffb617f51cfc1e3c061f93e9eb495a0cdae9a279
Fábio Ewerton <fabioew89@gmail.com>
2024-12-02 10:15:55 -0400

    feat: new page set_interface_unit

--- app/controllers/blueprints/network_routes.py
+++ app/controllers/blueprints/network_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask import Blueprint, request, render_template, flash
 from app.controllers.forms import Network_Form
 from app.controllers.netmiko import netmiko
 from app.models.model import Table_Register, Table_Devices
@@ -55,49 +55,34 @@ def get_interface_configuration():
     )
 
 
-# Rota: config_interface_unit
-@network_bp.route('/config_interface_unit', methods=['GET', 'POST'])
-def config_interface_unit():
-    users = db.session.execute(db.select(Table_Register)).scalars().all()
-    devices = db.session.execute(db.select(Table_Devices)).scalars().all()
-
-    output = None
-    if request.method == 'POST':
-        hostname = request.form.get('hostname')
-        username = request.form.get('username')
-        password = request.form.get('password')
-        unit = request.form.get('unit')
-
-        output = netmiko.get_interface_configuration(
-            hostname, username, password, unit
-            )
-
-    return render_template(
-        'router/config_interface_unit.html',
-        output=output,
-        users=users,
-        devices=devices,
-    )
-
-
+# Rota: set_interface_unit
 @network_bp.route('/set_interface_unit', methods=['GET', 'POST'])
 def set_interface_unit():
     form = Network_Form()
 
     output = None
 
+    # list users and devices in database
+    users = db.session.execute(
+        db.select(Table_Register).order_by(Table_Register.id)
+    ).scalars()
+
     devices = db.session.execute(
         db.select(Table_Devices).order_by(Table_Devices.id)
     ).scalars()
+
+    # create a tupla for users and devices
+    form.username.choices = [
+        (username.username, username.username) for username in users
+    ]
 
     form.device.choices = [
         (device.ip_address, device.hostname) for device in devices
     ]
 
     if form.validate_on_submit():
-        selected_device = form.device.data  # ignore_error
+        # selected_device = form.device.data
         flash('', )
-        return redirect(url_for('set_interface_unit'))
 
     return render_template(
         'router/set_interface_unit.html',
