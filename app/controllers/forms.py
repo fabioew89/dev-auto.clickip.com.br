8e764c72b5d1474550b491450e31bc69ab56d098
Fábio Ewerton <fabioew89@gmail.com>
2024-12-03 08:12:21 -0400

    fix: resolved conflicts in merge

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -1,10 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, \
     SelectField, IntegerField, FloatField
-
 from wtforms.validators import DataRequired, Length, EqualTo, \
     ValidationError, IPAddress, NumberRange
-
 import ipaddress
 from app import db
 from app.models.model import Table_Register
@@ -41,18 +39,18 @@ class Form_Register(FlaskForm):
 
 class Form_Login(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired()]
+        'Username', validators=[DataRequired()]
     )
     password = PasswordField(
-        'password', validators=[DataRequired()])
+        'Password', validators=[DataRequired()])
     submit = SubmitField('Entrar')
 
 
 class Form_Devices(FlaskForm):
     hostname = StringField(
-        'hostname', validators=[DataRequired(), Length(min=3, max=10)])
+        'Hostname', validators=[DataRequired(), Length(min=3, max=10)])
     ip_address = StringField(
-        'ip_address', validators=[DataRequired(), IPAddress()]
+        'IP Address', validators=[DataRequired(), IPAddress()]
     )
     submit = SubmitField('Cadastrar')
 
@@ -65,45 +63,19 @@ class Network_Form(FlaskForm):
         'Username', choices=[]
     )
     password = PasswordField(
-        'Password',     validators=[DataRequired()]
+        'Password', validators=[DataRequired()]
     )
     unit_vlan = IntegerField(
-<<<<<<< HEAD
-        'Unit VLAN',    validators=[
-            DataRequired(),
-            NumberRange(min=1, max=4096)
-        ]
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        'Unit VLAN', validators=[DataRequired()]
-=======
-=======
->>>>>>> Stashed changes
         'Unit VLAN', validators=[
             DataRequired(),
             NumberRange(min=1, max=4096)
         ]
->>>>>>> Stashed changes
->>>>>>> test
     )
     description = StringField(
-        'Description',  validators=[DataRequired()]
+        'Description', validators=[DataRequired()]
     )
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    bandwith = FloatField(
-<<<<<<< HEAD
-        'Bandwidth',    validators=[DataRequired()]
-=======
-=======
-    bandwidth = FloatField(
->>>>>>> Stashed changes
-=======
-    bandwidth = FloatField(
->>>>>>> Stashed changes
+    bandwidth = IntegerField(
         'Bandwidth', validators=[DataRequired()]
->>>>>>> test
     )
     ipv4_gw = StringField(
         'IPv4 Address', validators=[
@@ -114,47 +86,19 @@ class Network_Form(FlaskForm):
     ipv6_gw = StringField(
         'IPv6 Gateway', validators=[
             DataRequired(),
-            validate_ipv6
+            validate_ipv6()
         ]
     )
     ipv6_cli = StringField(
-<<<<<<< HEAD
-        'IPv6 Client',  validators=[DataRequired(), IPAddress(ipv6=True)]
-    )
-    ipv6_48 = StringField(
-        'IPv6 /48',     validators=[DataRequired(), IPAddress(ipv6=True)]
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        'IPv6 Client', validators=[DataRequired(), IPAddress(ipv6=True)]
-    )
-    ipv6_48 = StringField(
-        'IPv6 /48', validators=[DataRequired(), IPAddress(ipv6=True)]
-=======
         'IPv6 Client', validators=[
             DataRequired(),
-            validate_ipv6
+            validate_ipv6()
         ]
     )
     ipv6_48 = StringField(
-=======
-        'IPv6 Client', validators=[
-            DataRequired(),
-            validate_ipv6
-        ]
-    )
-    ipv6_48 = StringField(
->>>>>>> Stashed changes
         'IPv6 /48', validators=[
             DataRequired(),
-            validate_ipv6
+            validate_ipv6()
         ]
     )
-    submit = SubmitField(
-        'Enviar'
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
->>>>>>> test
-    )
+    submit = SubmitField('Enviar')
