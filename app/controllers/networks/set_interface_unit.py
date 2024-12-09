e70fd33e914bfcb7c72c321cf06c1647d067b7ea
Fábio Ewerton <fabioew89@gmail.com>
2024-12-09 15:42:08 -0400

    fix: add new line in set_interface_unit for view static route

--- app/controllers/networks/set_interface_unit.py
+++ app/controllers/networks/set_interface_unit.py
@@ -9,28 +9,37 @@ def set_interface_unit(hostname, username, password, unit, description,
         'username': username,
         'password': password,
     }
-    ssh = ConnectHandler(**router)
-    ssh.send_config_set(f'set interfaces ae0 unit {unit} description "{description}"                                                    ')  # noqa: E501
-    ssh.send_config_set(f'set interfaces ae0 unit {unit} vlan-id {unit}                                                                 ')  # noqa: E501
-    ssh.send_config_set(f'set interfaces ae0 unit {unit} family inet filter output PROTECT-CLIENTES                                     ')  # noqa: E501
-    ssh.send_config_set(f'set interfaces ae0 unit {unit} family inet policer input {bandwidth}mb-filter output {bandwidth}mb-filter     ')  # noqa: E501
-    ssh.send_config_set(f'set interfaces ae0 unit {unit} family inet sampling input output                                              ')  # noqa: E501
-    ssh.send_config_set(f'set interfaces ae0 unit {unit} family inet address {ipv4_gw}/30                                               ')  # noqa: E501
-    ssh.send_config_set(f'set interfaces ae0 unit {unit} family inet6 policer input {bandwidth}mb-filter output {bandwidth}mb-filter    ')  # noqa: E501
-    ssh.send_config_set(f'set interfaces ae0 unit {unit} family inet6 sampling input output                                             ')  # noqa: E501
-    ssh.send_config_set(f'set interfaces ae0 unit {unit} family inet6 address {ipv6_gw}/126                                             ')  # noqa: E501
-    ssh.send_config_set(f'set routing-options rib inet6.0 static route {inet6_48}/48 next-hop {ipv6_cli}                                ')  # noqa: E501
 
-    ssh.commit()  # not yet, but, soon
+    commands = [
+        f'set interfaces ae0 unit {unit} description "{description}"',
+        f'set interfaces ae0 unit {unit} vlan-id {unit}',
+        f'set interfaces ae0 unit {unit} family inet filter output PROTECT-CLIENTES',                                   # noqa: E501
+        f'set interfaces ae0 unit {unit} family inet policer input {bandwidth}mb-filter output {bandwidth}mb-filter',   # noqa: E501
+        f'set interfaces ae0 unit {unit} family inet sampling input output',
+        f'set interfaces ae0 unit {unit} family inet address {ipv4_gw}/30',
+        f'set interfaces ae0 unit {unit} family inet6 policer input {bandwidth}mb-filter output {bandwidth}mb-filter',  # noqa: E501
+        f'set interfaces ae0 unit {unit} family inet6 sampling input output',
+        f'set interfaces ae0 unit {unit} family inet6 address {ipv6_gw}/126',
+        f'set routing-options rib inet6.0 static route {inet6_48}/48 next-hop {ipv6_cli}',                              # noqa: E501
+    ]
 
-    output = ssh.send_command(f'run show configuration interfaces ae0 unit {unit}')  # noqa: E501
-    # output = ssh.send_config_set(f'show configuration interfaces ae0 unit {unit}') # noqa: E501
+    try:
+        ssh = ConnectHandler(**router)
+        ssh.send_config_set(commands)
+        ssh.commit()
+        output = [
+            ssh.send_command(f'run show configuration interfaces ae0 unit {unit}'),                                     # noqa: E501
+            ssh.send_command(f'run show configuration | display set | match {ipv6_cli}'),                               # noqa: E501
+        ]
 
-    # output = [
-    #     ssh.send_config_set(f'run show configuration interfaces ae0 unit {unit}\n'),                                                        # noqa: E501
-    #     ssh.send_command(f'run show configuration | display set | match {ipv6_cli}')                                                        # noqa: E501
-    # ]
+    except Exception as e:
+        print(f'Erro ao executar comandos: {e}')
+        output = None
 
-    ssh.disconnect()
+    finally:
+        ssh.disconnect()
 
-    return output
+    if output:
+        formatted_output = "\n".join(output)
+
+    return formatted_output
