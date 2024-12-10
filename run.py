126874698c4a9b6122a7d2a11d5ef828cf5bb3a5
Fábio Ewerton <fabioew89@gmail.com>
2024-12-10 17:16:53 -0400

    feat: add new function create_admin(app)

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from app import db, create_app
+from app.controllers.admin import create_admin
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
+
+create_admin(app)
 
 if __name__ == "__main__":
     app.run()
