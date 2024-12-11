99534f18c5a18f1c3e9f423468fcbcd872a3d654
Fábio Ewerton <fabioew89@gmail.com>
2024-12-11 16:39:42 -0400

    fix: clear spaces of the run.py

--- run.py
+++ run.py
@@ -1,12 +1,9 @@
 from app import db, create_app
-from app.controllers.admin import create_admin
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
-
-create_admin(app)
 
 if __name__ == "__main__":
     app.run()
