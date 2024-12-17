542386bf9e6b52d246b46e458aa0b0dfb512780c
Fábio Ewerton <fabioew89@gmail.com>
2024-12-17 15:16:06 -0400

    refact: changed mapped to Column traditional

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,4 @@
 from app import db, lm
-from sqlalchemy.orm import Mapped, mapped_column
 from flask_login import UserMixin
 
 
@@ -10,20 +9,19 @@ def load_user(user_id):
 
 class Users(db.Model, UserMixin):
     __tablename__ = 'Users'
-    id:         Mapped[int] = mapped_column(primary_key=True)
-    username:   Mapped[str] = mapped_column(unique=True, nullable=False)
-    password:   Mapped[str] = mapped_column(unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, nullable=True)
+    password = db.Column(db.String, nullable=True)
 
     def __str__(self):
-        return self.id, self.username
+        return super().__str__(self.username)
 
 
 class Devices(db.Model):
     __tablename__ = 'Devices'
-    id:         Mapped[int] = mapped_column(primary_key=True)
-    hostname:   Mapped[str] = mapped_column(unique=True, nullable=False)
-    ip_address: Mapped[str] = mapped_column(unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    hostname = db.Column(db.String, nullable=True)
+    ip_address = db.Column(db.String, nullable=True)
 
     def __str__(self):
-        return self.hostname, self.ip_address
-''
+        return super().__str__(self.hostname, self.ip_address)
