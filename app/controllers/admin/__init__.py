ab89c41a5553f8e11467adaff542a61ec0d8ca2b
Fábio Ewerton <fabioew89@gmail.com>
2024-12-17 15:15:56 -0400

    foo

--- app/controllers/admin/__init__.py
+++ app/controllers/admin/__init__.py
@@ -1,10 +1,9 @@
-from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from app import db
-from app.models.model import Table_Register, Table_Devices
+from app import db, admin
+from app.models import Users, Devices
 
 
-def create_admin(app):
-    admin = Admin(app, name='auto.clickip.local', template_mode='bootstrap3')
-    admin.add_view(ModelView(Table_Register, db.session))
-    admin.add_view(ModelView(Table_Devices, db.session))
+def create_admin():
+    admin.name = 'auto.clickip.local'
+    admin.add_view(ModelView(Users, db.session))
+    admin.add_view(ModelView(Devices, db.session))
