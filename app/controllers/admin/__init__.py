ffe8b66f60e1ef9820a85c029ec80307c3e5be89
Fábio Ewerton <fabioew89@gmail.com>
2024-12-17 17:28:47 -0400

    refac: refactored admin code

--- app/controllers/admin/__init__.py
+++ app/controllers/admin/__init__.py
@@ -1,6 +1,6 @@
-from flask_admin.contrib.sqla import ModelView
 from app import db, admin
 from app.models import Users, Devices
+from flask_admin.contrib.sqla import ModelView
 
 
 def create_admin():
