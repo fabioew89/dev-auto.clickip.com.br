59d34d4a4777ab1dfd3ab1c1ebd13891cc5ec0e9
Fábio Ewerton <fabioew89@gmail.com>
2024-12-17 14:51:44 -0400

    fix: app/models/model.py -> app/models/__init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,11 +5,11 @@ from flask_login import UserMixin
 
 @lm.user_loader
 def load_user(user_id):
-    return Table_Register.query.get(int(user_id))
+    return Users.query.get(int(user_id))
 
 
-class Table_Register(db.Model, UserMixin):
-    __tablename__ = 'Table_Register'
+class Users(db.Model, UserMixin):
+    __tablename__ = 'Users'
     id:         Mapped[int] = mapped_column(primary_key=True)
     username:   Mapped[str] = mapped_column(unique=True, nullable=False)
     password:   Mapped[str] = mapped_column(unique=True, nullable=False)
@@ -18,11 +18,12 @@ class Table_Register(db.Model, UserMixin):
         return self.id, self.username
 
 
-class Table_Devices(db.Model):
-    __tablename__ = 'Table_Devices'
+class Devices(db.Model):
+    __tablename__ = 'Devices'
     id:         Mapped[int] = mapped_column(primary_key=True)
     hostname:   Mapped[str] = mapped_column(unique=True, nullable=False)
     ip_address: Mapped[str] = mapped_column(unique=True, nullable=False)
 
     def __str__(self):
         return self.hostname, self.ip_address
+''
